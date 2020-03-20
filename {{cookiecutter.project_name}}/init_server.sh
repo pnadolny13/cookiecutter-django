3f5076167832951a565ceb6c9a68dc3719fbb017
Pat Nadolny <patnadolny@gmail.com>
2020-03-19 22:48:41 -0400

    fix docker install

--- {{cookiecutter.project_name}}/init_server.sh
+++ {{cookiecutter.project_name}}/init_server.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-ssh ec2-user@$1 "sudo yum update -y"
-ssh ec2-user@$1 "sudo amazon-linux-extras install docker"
-ssh ec2-user@$1 "sudo service docker start"
-ssh ec2-user@$1 "sudo usermod -a -G docker ec2-user"
-
-ssh ec2-user@$1 "sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose"
-ssh ec2-user@$1 "sudo chmod +x /usr/local/bin/docker-compose"
-ssh ec2-user@$1 "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
+sudo yum update -y;
+sudo amazon-linux-extras install docker;
+sudo service docker start;
+sudo usermod -a -G docker ec2-user;
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+sudo chmod +x /usr/local/bin/docker-compose;
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose;
